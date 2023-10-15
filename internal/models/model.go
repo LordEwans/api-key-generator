cfb35bb0e1e24b24d9a10c5e705adb5ff8cf89f8
Ewanretor Okugbe <ewanretorokugbe@gmail.com>
2023-10-15 01:16:11 +0100

    Fixes

--- internal/models/model.go
+++ internal/models/model.go
@@ -3,5 +3,5 @@ package models
 type User struct {
 	Username string `json:"username" validate:"required" bson:"username"`
 	APIKey   string `json:"api_key,omitempty" bson:"api_key"`
-	API      string `json:"api,omitempty" validate:"required" bson:"api_key"`
+	API      string `json:"api,omitempty" validate:"required" bson:"api"`
 }
